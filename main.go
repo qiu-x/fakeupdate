acb6f392a6c08dc07512d50e20c5f2e67c036c7c
Qiu <alex@alexslomka.xyz>
2022-05-29 12:46:19 +0200

    Change OpenGL version (4.1 -> 3.3)

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/gl//v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/go-gl/mathgl/mgl32"
 
